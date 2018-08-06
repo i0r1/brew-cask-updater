f17e43ca155c815d1da536185bcca1efad0d0673
iori <iori7891@gmail.com>
2018-08-05 22:32:57 -0300

    First Commit

---
+++ updater.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for c in $(brew cask list); do
+    info=$(brew cask info $c)
+    latest=$(echo "$info" | head -1 | cut -d ' ' -f2)
+    installed=$(echo "$info" | sed -n '3p' | awk '{print $(1)}' | awk -F '/' '{print $NF}')
+    if [ "$installed" != "$latest" ]; then
+        echo "$c is installed '$installed', latest is '$latest'"
+         
+        if [ "$1" == "up" ]; then
+          brew cask reinstall $c
+       fi 
+    fi
+done
+
