68d61ff1a6e7a13352a679e667fb75e4e6535f04
iori <23272304+i0r1@users.noreply.github.com>
2018-12-24 13:01:37 -0400

    change params

--- updater.sh
+++ updater.sh
@@ -5,9 +5,9 @@ for c in $(brew cask list); do
     latest=$(echo "$info" | head -1 | cut -d ' ' -f2)
     installed=$(echo "$info" | sed -n '3p' | awk '{print $(1)}' | awk -F '/' '{print $NF}')
     if [ "$installed" != "$latest" ]; then
-        echo "$c is installed '$installed', latest is '$latest'"
+        echo "$c installed is '$installed', latest is '$latest'"
          
-        if [ "$1" == "up" ]; then
+        if [ "$1" != "ls" ]; then
           brew cask reinstall $c
        fi 
     fi
